707bb6dd1cf8d7602215ea47bb530120fe740cac
genzouw <genzouw@gmail.com>
2019-11-30 23:17:10 +0900

    first commit.

---
+++ main.js
@@ -0,0 +1,33 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    numberOfFaces: 6,
+    totalNumberOfTrials: 100,
+    dice: [],
+    isRunning: false,
+    speed: 1,
+  },
+  methods: {
+    execute: function () {
+      this.dice = Array(parseInt(this.numberOfFaces)).fill(0)
+
+      this.isRunning = true
+
+      let timer = setInterval(() => {
+        if (this.cumulativeNumberOfTrials() >= this.totalNumberOfTrials) {
+          clearInterval(timer)
+          this.isRunning = false
+          return
+        }
+
+        this.dice[parseInt(Math.random() * this.dice.length)]++
+
+        app.$forceUpdate();
+
+      }, this.speed)
+    },
+    cumulativeNumberOfTrials: function () {
+      return this.dice.reduce((s, i) => { return s + i }, 0)
+    }
+  }
+})
